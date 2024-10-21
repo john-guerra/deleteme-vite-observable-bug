009303a2f7a34e22fb479b74e8174270ea039419
John Alexis Guerra Gómez <john.guerra@gmail.com>
2024-10-21 12:23:26 -0700

    Basic code to demonstrate error

---
+++ README.md
@@ -0,0 +1,1 @@
+Vite Observable Module Bug
---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script type="module">
+  import { Runtime, Inspector } from "@observablehq/runtime";
+  import define from "@fil/druidjs-worker";
+
+  const notebook = new Runtime().module(define, (name) => {
+    return ["result"].includes(name);      
+  });
+
+  $: result = null;
+
+  notebook.value("result").then((value) => {
+    result = value;
+  });
+  
+</script>
+
+<div id="observablehq-result-a0a5b4b3">${JSON.stringify(result)}</div>
+<p>
+  Credit: <a href="https://observablehq.com/@fil/druidjs-worker"
+    >DruidJS worker by Fil</a
+  >
+</p>
+
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/@observablehq/inspector@5/dist/inspector.css"
+/>
